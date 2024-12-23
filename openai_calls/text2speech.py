c10ac3066b18f85d658a6ac48b90b3734c36cb2a
alana <alana@ai21.com>
2024-12-23 16:51:19 +0200

    trying

--- openai_calls/text2speech.py
+++ openai_calls/text2speech.py
@@ -27,7 +27,10 @@ def play_voice(text):
         pass
 
     # Clean up the file
-    os.remove(temp_audio_path)
+    try:
+        os.remove(temp_audio_path)
+    except:
+        pass
 
 
 if __name__ == "__main__":
