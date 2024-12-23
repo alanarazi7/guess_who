00b74dd28b42fc75aaa5e1c4fb72d0597e53a41b
alana <alana@ai21.com>
2024-12-24 01:16:31 +0200

    removing some useless files

--- utils.py
+++ utils.py
@@ -1,10 +1,2 @@
-from datetime import datetime
-
-
-def print_ts(s: str):
-    now_ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[{now_ts}] {s}")
-
-
 def normalize_str(s: str):
     return s.lower().replace(".", '').strip()
